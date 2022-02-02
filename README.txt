298f74ba74b0e20afc3b7ed432c80c5726013dc0
Dávid Mezőfi <david.mezofi93@gmail.com>
2022-02-02 17:50:49 +0100

    Add the files

---
+++ README.txt
@@ -0,0 +1,33 @@
+Copyright 2022 Dávid Csaba Mezőfi
+
+Copying and distribution of this file, with or without modification, are
+permitted in any medium without royalty provided the copyright notice and this
+notice are preserved.  This file is offered as-is, without any warranty.
+
+
+                                   PUNCH.SH
+
+The shells script punch.sh automates the process of installing fonts for groff.
+
+    punch.sh [-h] [-g groffpath] [-v groffversion] [-f fontpath] fontfile fontname
+
+The first argument fontfile shall be a TrueType (TTF), OpenType (OTF) or Type 1
+(PFA) font file.  The second argument fontname shall be the groff font name,
+e.g. MyFontR.  The recommended naming convention is to use the letters R, B, I
+and BI as postfixes in fontname for roman, italic, bold and bold italic font
+faces, respectively.  In the example above MyFontR would stand for the roman
+font of the MyFont font family.
+
+By default the script assumes /usr/share/groff/1.22.4 to be the location of
+your groff installation (groffpath/groffversion) and /etc/groff/site-font to be
+the first directory in your font path (fontpath).
+
+Most likely the script needs to be run as root, but it depends on your rights
+to the directories where you want to install the fonts.
+
+For a complete manual run
+
+    groff -man -Tutf8 punch.sh.1 | less -R
+
+The script was inspired by the install-font.sh script
+<https://www.schaffter.ca/mom/momdoc/appendices.html#fonts> by Peter Schaffter.
---
+++ punch.sh
@@ -0,0 +1,187 @@
+#! /bin/sh
+
+# Copyright 2022 Dávid Csaba Mezőfi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+GROFFPATH=/usr/share/groff
+GROFFVERSION=1.22.4
+FONTPATH=/etc/groff/site-font
+
+usage() {
+    echo "Usage: punch.sh [-h] [-g groffpath] [-v groffversion] [-f fontpath] fontfile fontname" >&2
+}
+
+TEMP=$(getopt -o '+hg:v:f:' -n 'davidfont.sh' -- "$@")
+[ $? -ne 0 ] && {
+    echo "punch.sh: Error during parsing options" >&2 
+    exit 1
+}
+eval set -- "$TEMP"
+unset TEMP
+while true
+do
+    case "$1" in
+        '-h')
+            usage
+            exit 0
+            ;;
+        '-g')
+            GROFFPATH="$2"
+            shift 2
+            break
+            ;;
+        '-v')
+            GROFFVERSION="$2"
+            shift 2
+            break
+            ;;
+        '-f')
+            FONTPATH="$2"
+            shift 2
+            break
+            ;;
+        '--')
+            shift
+            break
+            ;;
+        *)
+            echo "punch.sh: Error during parsing options" >&2 
+            usage
+            exit 1
+    esac
+done
+
+isitthere () {
+    [ ! -e "$1" ] && {
+        echo "punch.sh: $1 does not exist." >&2
+        exit 3
+    }
+}
+
+isitthere "$GROFFPATH"
+isitthere "$GROFFPATH/$GROFFVERSION"
+isitthere "$FONTPATH"
+
+[ $# -ne 2 ] && {
+    echo "punch.sh: Exactly two arguments are expected." >&2
+    usage
+    exit 1
+}
+
+isitthere "$1"
+case ${1##*.} in
+    "ttf" | "TTF" | "otf" | "OTF" | "pfb" | "PFB" );;
+    *)
+        echo "punch.sh: The argument FONT must a TTF, OTF or PFB font." >&2
+        usage
+        exit 1
+esac
+FONTFILE=$(readlink -f "$1")
+
+which fontforge > /dev/null 2>&1 || {
+    echo "punch.sh: fontforge is not in \$PATH." >&2
+    exit 2
+}
+
+TMPDIR=/tmp/fontbuild
+[ -d $TMPDIR ] && {
+    echo "punch.sh: $TMPDIR already exists."
+    exit 4
+}
+mkdir -p $TMPDIR
+
+# See https://www.schaffter.ca/mom/momdoc/appendices.html#fonts
+# Create the generate-t42.pe script used for TrueType and OpenType fonts
+cat << EOF > $TMPDIR/generate-t42.pe
+# generate-t42.pe
+
+Open(\$1);
+Generate(\$fontname + ".pfa");
+Generate(\$fontname + ".t42");
+EOF
+
+# Create the generate-pfa.pe script used for Type1 fonts
+cat << EOF > $TMPDIR/generate-pfa.pe
+# generate-pfa.pe
+
+Open(\$1);
+Generate(\$fontname + ".pfa");
+EOF
+
+TOTF=0
+PFA=0
+case ${1##*.} in
+    "ttf" | "TTF" | "otf" | "OTF")
+        TOTF=1
+        fontforge -script "$TMPDIR/generate-t42.pe" "$FONTFILE"
+        ;;
+    "pfb" | "PFB" )
+        PFA=1
+        fontforge -script "$TMPDIR/generate-pfa.pe" "$FONTFILE"
+        ;;
+esac
+
+FONTNAME=$(sed -n -e '/^[fF]ont[nN]ame \+/{s/^[fF]ont[nN]ame \+\(.*\)$/\1/;p}' ./*.afm)
+isitthere "$FONTNAME.afm"
+isitthere "$FONTNAME.pfa"
+mv "$FONTNAME.pfa" "$FONTNAME.afm" "$TMPDIR/"
+[ $TOTF -eq 1 ] && {
+    isitthere "$FONTNAME.t42"
+    mv "$FONTNAME.t42" "$TMPDIR/"
+}
+
+isitalic=0
+grep -e '^[fF]ont[nN]ame \+.*[iI]talic' "$TMPDIR/$FONTNAME.afm" && isitalic=1
+
+# See the man page of afmtodit for a description of its options
+TEXTMAP="$GROFFPATH/$GROFFVERSION/font/devps/generate/textmap"
+TEXTENC="$GROFFPATH/$GROFFVERSION/font/devps/text.enc"
+[ "$isitalic" -eq 0 ] && \
+    afmtodit -e "$TEXTENC" -i0 -m -o "$TMPDIR/$2" "$TMPDIR/$FONTNAME.afm" "$TEXTMAP" "$2"
+[ "$isitalic" -eq 1 ] && \
+    afmtodit -e "$TEXTENC" -i50 -o "$TMPDIR/$2" "$TMPDIR/$FONTNAME.afm" "$TEXTMAP" "$2"
+isitthere "$TMPDIR/$2"
+
+# See the man page of grops and gropdf
+isitthere "$FONTPATH/devps"
+isitthere "$FONTPATH/devpdf"
+
+[ $TOTF -eq 1 ] && {
+    mv -f "$TMPDIR/$FONTNAME.t42" "$TMPDIR/$2" "$FONTPATH/devps/"
+    mv -f "$TMPDIR/$FONTNAME.pfa" "$FONTPATH/devpdf/"
+}
+[ $PFA -eq 1 ] && {
+    mv -f "$TMPDIR/$FONTNAME.pfa" "$TMPDIR/$2" "$FONTPATH/devps/"
+    ln -sf "$FONTPATH/devps/$FONTNAME.pfa" "$FONTPATH/devpdf/$FONTNAME.pfa" 
+}
+ln -sf "$FONTPATH/devps/$2" "$FONTPATH/devpdf/$2" 
+
+downloadbkp() {
+    [ -e "$1" ] && \
+        cp --backup=numbered "$1" "$1.bkp"
+    [ ! -e "$1" ] && {
+        isitthere "$2"
+        cp "$2" "$1"
+    }
+}
+
+downloadbkp "$FONTPATH/devps/download" "$GROFFPATH/$GROFFVERSION/font/devps/download"
+downloadbkp "$FONTPATH/devpdf/download" "$GROFFPATH/$GROFFVERSION/font/devpdf/download"
+
+[ $TOTF -eq 1 ] && printf "%s\t%s.t42\n" "$FONTNAME" "$FONTNAME" >> "$FONTPATH/devps/download"
+[ $PFA -eq 1 ] && printf "%s\t%s.pfa\n" "$FONTNAME" "$FONTNAME" >> "$FONTPATH/devps/download"
+printf "\t%s\t%s.pfa\n" "$FONTNAME" "$FONTNAME" >> "$FONTPATH/devpdf/download"
+
+rm -rf "$TMPDIR"
+exit 0
